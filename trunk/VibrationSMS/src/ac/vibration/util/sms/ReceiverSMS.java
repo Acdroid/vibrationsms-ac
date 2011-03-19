e04e01b9a59f89334e98760f0c8f17fae4072c96
aracemcskyline@gmail.com <aracemcskyline@gmail.com@10d2930d-91fe-c36b-2414-62206aa3c91b>
2011-03-19 17:13:18 +0000

    Actualizacion del manifest para recibir sms Version 0.1 del receptor de sms, ahora vibra al recibir SMS

--- trunk/VibrationSMS/src/ac/vibration/util/sms/ReceiverSMS.java
+++ trunk/VibrationSMS/src/ac/vibration/util/sms/ReceiverSMS.java
@@ -7,15 +7,14 @@
 package ac.vibration.util.sms;
 
 import ac.vibration.util.Vibration.DoVibration;
-import android.app.Notification;
 import android.app.NotificationManager;
-import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Vibrator;
 import android.telephony.SmsMessage;
+import android.widget.Toast;
 
 /**
  * ReceiverSMS.java 09/03/2011
@@ -23,15 +22,16 @@ import android.telephony.SmsMessage;
  */
 public class ReceiverSMS extends BroadcastReceiver {
 	
-	public static String KEY_SMS = "SMS";
-	public static String KEY_CONTACTO = "CONTACTO";
-	
-	private NotificationManager mNotificationManager;
-	private int SIMPLE_NOTFICATION_ID = 1984;
-	
 	private String numTelf[];
 	private int index = 0; //index numeros telefono
 
+	
+//	public static String KEY_SMS = "SMS";
+//	public static String KEY_CONTACTO = "CONTACTO";
+//	private NotificationManager mNotificationManager;
+//	private int SIMPLE_NOTFICATION_ID = 1984;
+//	
+	
 
 	/* (non-Javadoc)
 	 * @see android.content.BroadcastReceiver#onReceive(android.content.Context, android.content.Intent)
@@ -47,33 +47,44 @@ public class ReceiverSMS extends BroadcastReceiver {
 		{
 			//---Recuperamos el mensaje---
 			// PDU = protocol discription unit
-			
 			Object[] pdus = (Object[]) bundle.get("pdus");
 			msgs = new SmsMessage[pdus.length];            
+			numTelf= new String[pdus.length];
+			//Extraemos los numeros de telefono
 			for (int i=0; i<msgs.length; i++){
 				msgs[i] = SmsMessage.createFromPdu((byte[])pdus[i]);                
 				numTelf [index] += msgs[i].getOriginatingAddress();    
 				index++;
 			}
 			//---Mostrar el mensaje del SMS---
-			//Toast.makeText(context, str, Toast.LENGTH_SHORT).show();
-			DoVibration.SMS((Vibrator) mContext.getSystemService(Context.VIBRATOR_SERVICE));
-			creaNotification(textoSMS,mContext,msgs[0].getOriginatingAddress());
+			
+			//Para cada telefono recibido comprobamos si tiene una vibracion personificada
+			for (int i=0 ; i<numTelf.length ; i ++){
+				
+				//TODO codigo para comprobar usuarios
+				
+				
+				Toast.makeText(mContext, "Mensaje de " + numTelf[i], Toast.LENGTH_SHORT).show();
+				DoVibration.SMS((Vibrator) mContext.getSystemService(Context.VIBRATOR_SERVICE));
+			}
+			
+			
+			//creaNotification(textoSMS,mContext,msgs[0].getOriginatingAddress());
 
 		}
 	}
 
-	private void creaNotification(String textSMS,Context mContext,String contacto){
-		mNotificationManager = (NotificationManager)mContext.getSystemService(Context.NOTIFICATION_SERVICE);
-        Notification notifyDetails = new Notification(R.drawable.tacticlogo2,"Nuevo SMS",System.currentTimeMillis());
-        Intent i = new Intent (mContext,LeerSMS.class);
-        i.putExtra(KEY_SMS, textSMS);
-        i.putExtra(KEY_CONTACTO, contacto);        
-        //PendingIntent myIntent = PendingIntent.getActivity(mContext, 0, new Intent(mContext, SMSNuevosViejos.class), PendingIntent.FLAG_UPDATE_CURRENT);
-        PendingIntent myIntent = PendingIntent.getActivity(mContext, 0, i, 0);
-        notifyDetails.setLatestEventInfo(mContext, textSMS, "Pulsa para abrir", myIntent);
-        notifyDetails.flags |= Notification.FLAG_AUTO_CANCEL;
-        mNotificationManager.notify(SIMPLE_NOTFICATION_ID, notifyDetails);
-	}
+//	private void creaNotification(String textSMS,Context mContext,String contacto){
+//		mNotificationManager = (NotificationManager)mContext.getSystemService(Context.NOTIFICATION_SERVICE);
+//        Notification notifyDetails = new Notification(R.drawable.tacticlogo2,"Nuevo SMS",System.currentTimeMillis());
+//        Intent i = new Intent (mContext,LeerSMS.class);
+//        i.putExtra(KEY_SMS, textSMS);
+//        i.putExtra(KEY_CONTACTO, contacto);        
+//        //PendingIntent myIntent = PendingIntent.getActivity(mContext, 0, new Intent(mContext, SMSNuevosViejos.class), PendingIntent.FLAG_UPDATE_CURRENT);
+//        PendingIntent myIntent = PendingIntent.getActivity(mContext, 0, i, 0);
+//        notifyDetails.setLatestEventInfo(mContext, textSMS, "Pulsa para abrir", myIntent);
+//        notifyDetails.flags |= Notification.FLAG_AUTO_CANCEL;
+//        mNotificationManager.notify(SIMPLE_NOTFICATION_ID, notifyDetails);
+//	}
 
 }
