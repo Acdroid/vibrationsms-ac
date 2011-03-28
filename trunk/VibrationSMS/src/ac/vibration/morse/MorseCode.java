50866f35e83b992debec08fb2c5ceee97660c3cb
cskyline@gmail.com <cskyline@gmail.com@10d2930d-91fe-c36b-2414-62206aa3c91b>
2011-03-28 21:25:45 +0000

    Movido limpiarTexto a utils.tools.

--- trunk/VibrationSMS/src/ac/vibration/morse/MorseCode.java
+++ trunk/VibrationSMS/src/ac/vibration/morse/MorseCode.java
@@ -131,7 +131,6 @@ public class MorseCode {
 		if (s == null) return null;		
 		if (s.length() == 0) return "";
 		
-		s = limpiaTexto(s);
 		String ret = "";
 		
 		//Recorremos la cadena, los carateres erroneos se ignoraran
@@ -204,68 +203,6 @@ public class MorseCode {
 	
 		return false;
 	}
-	
-	
-	/**
-	 * <ul>Devuelve el mismo texto pero habiendo eliminado 
-	 * todos los simbolos que no deseemos tales como parentesis, llaves,
-	 * corchetes etc.
-	 * Se elimina acentos y dieresis.</ul><br><br>
-	 * @param text Texto del cual queremos eliminar los simbolos.
-	 * @return Texto parseado sin simbolos.
-	 */
-	public static String limpiaTexto(String text){
-
-		//Si se queren añadir simbolos que aparezcan en el texto restultado
-		//agregar aqui
-		String resultado = "";
-		String aux = text.toLowerCase(); 
-		
-		for (int i=0; i < aux.length(); i++){
-			if ( ((int)(aux.charAt(i)) < 65) ||( (((int)(aux.charAt(i) )) > 90)  && (((int)(aux.charAt(i) )) < 97))
-					|| ((int)(aux.charAt(i)) > 122)   ){
-				//Comprueba si es una vocal con acento, espacio o signo de puntuacion
-				switch(aux.charAt(i))
-				{
-				case ('\u00e1'): resultado += "a";
-				break;
-				case ('\u00e9'): resultado +="e";
-				break;
-				case ('\u00ed'): resultado +="i";
-				break;
-				case ('\u00f3'): resultado +="o";
-				break;
-				case ('\u00fa'): resultado +="u";
-				break;
-				case ('\u00FC'): resultado +="u";
-				break;
-				case ('\u00f1'): resultado += "n";
-				break;
-				default : resultado +=aux.charAt(i);   //De momento para todos los elementos extranos
-				}
-			}
-			else
-				resultado += aux.charAt(i);
-		}
-		
-
-		resultado = resultado.replaceAll("[^\\p{L}\\p{N}\\ ]","");
-
-		//Transformamos numeros a letras
-//		resultado = resultado.replaceAll("[0]", "cero ");
-//		resultado = resultado.replaceAll("[1]", "uno ");
-//		resultado = resultado.replaceAll("[2]", "dos ");
-//		resultado = resultado.replaceAll("[3]", "tres ");
-//		resultado = resultado.replaceAll("[4]", "cuatro ");
-//		resultado = resultado.replaceAll("[5]", "cinco ");
-//		resultado = resultado.replaceAll("[6]", "seis ");
-//		resultado = resultado.replaceAll("[7]", "siete ");
-//		resultado = resultado.replaceAll("[8]", "ocho ");
-//		resultado = resultado.replaceAll("[9]", "nueve ");
-
-		return resultado;
-	}
-	
 	
 	
 }
